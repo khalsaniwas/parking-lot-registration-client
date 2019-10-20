297aeddbc5456b5ef76aa3fd1a530dc01e69f39c
khalsaniwas <honeysaini21@gmail.com>
2019-10-19 22:21:55 -0400

    Create new vehicle to register with signed in user

--- assets/scripts/vehicle/events.js
+++ assets/scripts/vehicle/events.js
@@ -1,1 +1,17 @@
 'use strict'
+const getFormFields = require('../../../lib/get-form-fields.js')
+const api = require('./api.js')
+const ui = require('./ui.js')
+
+const onAddVehicle = function (event) {
+  event.preventDefault()
+  const form = event.target
+  const formData = getFormFields(form)
+  api.addVehicle(formData)
+    .then(ui.onAddVehicleSuccess)
+    .catch(ui.onAddVehicleFailure)
+}
+
+module.exports = {
+  onAddVehicle
+}
--- assets/scripts/vehicle/ui.js
+++ assets/scripts/vehicle/ui.js
@@ -1,1 +1,31 @@
 'use strict'
+
+const store = require('../store')
+
+const successMessage = function (newText) {
+  $('#message').text(newText)
+  $('#message').removeClass('alert-warning')
+  $('#message').addClass('alert-success')
+}
+
+const failureMessage = function (newText) {
+  $('#message').text(newText)
+  $('#message').removeClass('alert-success')
+  $('#message').addClass('alert-warning')
+}
+
+const onAddVehicleSuccess = function () {
+  successMessage('Vehicle created successfully')
+  $('#add-vehicle').trigger('reset')
+}
+
+const onAddVehicleFailure = function () {
+  failureMessage('Failed to create vehicle')
+  $('#add-vehicle').trigger('reset')
+}
+
+
+module.exports = {
+  onAddVehicleSuccess,
+  onAddVehicleFailure
+}
